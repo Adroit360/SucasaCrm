ab8ec05b83e17511505ad0b1f72f3ad76cfad550
Moro-Afriyie <ceremoinc99@gmail.com>
2022-10-31 09:18:05 +0000

    feat:create the various pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AccountsComponent } from './pages/accounts/accounts.component';
+import { CustomersComponent } from './pages/customers/customers.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { InquiriesComponent } from './pages/inquiries/inquiries.component';
+import { PropertiesComponent } from './pages/properties/properties.component';
+import { UnitsComponent } from './pages/units/units.component';
+import { ListingsComponent } from './pages/listings/listings.component';
+import { OpenHousesComponent } from './pages/open-houses/open-houses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    AccountsComponent,
+    CustomersComponent,
+    ContactsComponent,
+    InquiriesComponent,
+    PropertiesComponent,
+    UnitsComponent,
+    ListingsComponent,
+    OpenHousesComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/open-houses/open-houses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-open-houses',
+  templateUrl: './open-houses.component.html',
+  styleUrls: ['./open-houses.component.scss']
+})
+export class OpenHousesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
